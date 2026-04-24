3acde06fef07311ac81847177053142ad8c53d5c
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-24 16:05:07 +0700

    implement search functionality using pointers and add delnote method for node deletion

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -59,5 +59,19 @@ public:
     return (*current != NULL);
   }
 
+  bool delnote(int nim){
+    node *current, *previous;
+    if (!search(nim, &previous, &current))
+    return false;
+
+    if (current == start)
+        start = start->next;
+    else
+      previous->next = current->next;
+
+    delete current;
+    return true;
+  }
+
 
 };
